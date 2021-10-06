551db9aef127d1aa01d7bf9c9f169434e4dc573c
paimdev <bathkelucas@gmail.com>
2021-10-06 17:32:07 -0300

    Add rendering logic

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
 (function () {
   const gameBoard = {
-    gameBoard: [1,2,3,4,5,6,7,8,9],
+    gameBoard: ["","","","","","","","",""],
   };
 
   const Players = function (name, marker, winState=false) {
     return {name, marker, winState};
   };
 
+  const render = () => {
+    const board = this.gameBoard;
+    const gameContainer = document.querySelector('.game-container');
 
+    board.forEach(element => {
+      const square = document.createElement('div');
+      gameContainer.appendChild(square);
+    });
+  };
 
-  const gameplay = (function() {
+  const gameplay = function() {
     const player1 = Players("Josh", "X");
     const player2 = Players("Joran", "O");
 
@@ -21,7 +29,7 @@
       player2.play();
       checkWin();
     }
-  })();
+  };
 
 
 })();
