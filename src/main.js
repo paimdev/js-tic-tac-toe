16923dc92da65ba110d89c8c05df0d3f52b5e23d
paimdev <bathkelucas@gmail.com>
2021-10-07 13:08:15 -0300

    Clear code and rethink life decisions

--- src/main.js
+++ src/main.js
@@ -7,46 +7,8 @@ const game = (function () {
     return { name, marker, winState };
   };
 
-  Players.prototype.play = function (i) {
-    if (gameBoard.board[i] === "") {
-      gameBoard.board = this.marker;
-      render();
-    } else {
-      return invalidPlay(this);
-    }
-  };
+  
 
-  const render = (function () {
-    const board = gameBoard.board;
-    const boardContainer = document.querySelector(".board-container");
-
-    board.forEach((element) => {
-      const square = document.createElement("div");
-      square.classList.add("square-marker");
-      square.innerText = element;
-      boardContainer.appendChild(square);
-    });
-  })();
-
-  const listeners = (function () {
-    const squares = document.querySelectorAll(".square-marker");
-    for (const square of squares) {
-      square.addEventListener("click", () => {
-        gameBoard.board[i] = 'X'
-        render();
-      });
-    };
-  })();
-
-  const gameplay = () => {
-    const player1 = Players("Josh", "X");
-    const player2 = Players("Joran", "O");
-
-    while (true) {
-      player1.play();
-      checkWin();
-      player2.play();
-      checkWin();
-    }
-  };
 })();
+
+game.render();
