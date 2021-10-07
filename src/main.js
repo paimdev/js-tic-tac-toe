44c9e6531b2eeb1fa3c91353bc2a1b03c47edf29
paimdev <bathkelucas@gmail.com>
2021-10-07 14:20:17 -0300

    Setup the render function

--- src/main.js
+++ src/main.js
@@ -7,8 +7,26 @@ const game = (function () {
     return { name, marker, winState };
   };
 
-  
+  const render = function () {
+    const board = gameBoard.board;
+    const boardContainer = document.querySelector(".board-container");
+    let index = 0;
 
+    for (const area of board) {
+      const square = document.createElement("div");
+      square.textContent = area;
+      square.classList.add("marker-area");
+      square.setAttribute("id", index);
+      boardContainer.appendChild(square);
+      index++;
+    }
+  };
+
+  const placeMark = function (marker, index) {};
+
+  const gameplay = function () {};
+
+  return { render };
 })();
 
 game.render();
