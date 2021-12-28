0fc77afda5d56f86dfb330d856508844ba0c37de
paimdev <bathkelucas@gmail.com>
2021-12-28 12:35:04 -0300

    Restart project

--- src/main.js
+++ src/main.js
@@ -5,63 +5,6 @@ const game = (function () {
 
   currentMarker = "X";
 
-  const winningPositions = function () {
-    const board = gameBoard.board;
-
-    const horizontal = function () {
-      if (board[0] == "X" && board[1] == "X" && board[2] == "X") {
-        return true;
-      } else if (board[3] == "X" && board[4] == "X" && board[5] == "X") {
-        return true;
-      } else if (board[6] == "X" && board[7] == "X" && board[8] == "X") {
-        return true;
-      } else if (board[0] == "O" && board[1] == "O" && board[2] == "O") {
-        return true;
-      } else if (board[3] == "O" && board[4] == "O" && board[5] == "O") {
-        return true;
-      } else if (board[6] == "O" && board[7] == "O" && board[8] == "O") {
-        return true;
-      } else {
-        return false;
-      }
-    };
-    const vertical = function () {
-      if (board[0] == "X" && board[3] == "X" && board[6] == "X") {
-        return true;
-      } else if (board[1] == "X" && board[4] == "X" && board[7] == "X") {
-        return true;
-      } else if (board[2] == "X" && board[5] == "X" && board[8] == "X") {
-        return true;
-      } else if (board[0] == "O" && board[3] == "O" && board[6] == "O") {
-        return true;
-      } else if (board[1] == "O" && board[4] == "O" && board[7] == "O") {
-        return true;
-      } else if (board[2] == "O" && board[5] == "O" && board[8] == "O") {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    const diagonal = function () {
-      if (board[0] == "X" && board[4] == "X" && board[8] == "X") {
-        return true;
-      } else if (board[2] == "X" && board[4] == "X" && board[6] == "X") {
-        return true;
-      } else if (board[0] == "O" && board[4] == "O" && board[8] == "O") {
-        return true;
-      } else if (board[2] == "O" && board[4] == "O" && board[6] == "O") {
-        return true;
-      } else {
-        return false;
-      }
-    };
-  };
-
-  const Players = function (name, marker, winState = false) {
-    return { name, marker, winState };
-  };
-
   const render = function () {
     clearBoard();
     const board = gameBoard.board;
@@ -96,17 +39,10 @@ const game = (function () {
         placeMark(currentMarker, id);
         render();
         nextPlayer();
+        if (winningPositions.horizontal || winningPositions.vertical || winningPositions.diagonal) {
+          console.log("the game was won");
+        };
       });
-    }
-  };
-
-  const placeMark = function (marker, index) {
-    let board = gameBoard.board;
-
-    if (board[index] === "") {
-      board[index] = marker;
-    } else {
-      invalidPlay();
     }
   };
 
@@ -115,13 +51,6 @@ const game = (function () {
       currentMarker = "O";
     } else {
       currentMarker = "X";
-    }
-  };
-
-  const gameWin = function () {
-    const positions = winningPositions;
-    if (positions.horizontal || positions.vertical || positions.diagonal) {
-      return true;
     }
   };
 
